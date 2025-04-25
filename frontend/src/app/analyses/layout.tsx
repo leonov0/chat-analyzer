265e2810569d11544fe80c5268beb49ede601a16
Artyom Leonov <6leonov@gmail.com>
2025-04-25 12:23:43 +0300

    feat: integrate use analyses hook to fetch analyses data

--- frontend/src/app/analyses/layout.tsx
+++ frontend/src/app/analyses/layout.tsx
@@ -1,29 +1,19 @@
+"use client";
+
 import { AnalysesSidebar } from "@/components/analyses-sidebar";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { useAnalyses } from "@/features/analyses";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const analyses = [
-    {
-      id: "1",
-      name: "Chat 1",
-      created_at: new Date().toISOString(),
-      updated_at: new Date().toISOString(),
-    },
-    {
-      id: "2",
-      name: "Chat 2",
-      created_at: new Date().toISOString(),
-      updated_at: new Date().toISOString(),
-    },
-  ];
+  const { analyses, isLoading } = useAnalyses();
 
   return (
     <SidebarProvider className="max-w-screen">
-      <AnalysesSidebar analyses={analyses} />
+      <AnalysesSidebar analyses={analyses} isLoading={isLoading} />
       <SidebarInset className="space-y-4 overflow-auto p-4">
         <SidebarTrigger />
         {children}
