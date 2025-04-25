c2c03ce9d149dedabdbd2858ec738ed6f57b4c1f
Artyom Leonov <6leonov@gmail.com>
2025-04-25 12:23:21 +0300

    feat: create select chat page

--- frontend/src/app/analyses/page.tsx
+++ frontend/src/app/analyses/page.tsx
@@ -1,9 +1,9 @@
-"use client";
-
-import { useAuth } from "@/features/auth";
-
 export default function Chats() {
-  useAuth("auth");
-
-  return <div></div>;
+  return (
+    <div className="grid h-full place-items-center">
+      <h1 className="text-muted-foreground text-lg">
+        Select an analysis to view
+      </h1>
+    </div>
+  );
 }
