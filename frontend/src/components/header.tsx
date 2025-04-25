dd9ba3c39a2ce212e7e00b9f4ae8934ad496e476
Artyom Leonov <6leonov@gmail.com>
2025-04-25 09:42:56 +0300

    feat: add header and footer components

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { routes } from "@/lib/routes";
+
+export function Header() {
+  return (
+    <header className="bg-background/60 sticky top-0 z-50 py-4 backdrop-blur-sm">
+      <div className="container mx-auto flex justify-between gap-8">
+        <Link href={routes.home} className="font-medium">
+          ChatAnalyzer
+        </Link>
+
+        <nav>
+          <ul className="flex items-center gap-4">
+            <li>
+              <Button asChild variant="secondary">
+                <Link href={routes.register}>Sign Up</Link>
+              </Button>
+            </li>
+
+            <li>
+              <Button asChild>
+                <Link href={routes.login}>Log In</Link>
+              </Button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
