8aa53ef83934dec7b6509e0e93c0111f0f749cfd
Artyom Leonov <61983418+leonov0@users.noreply.github.com>
2025-06-03 07:04:42 +0300

    chore: add deploy on heroku workflow (#7)
    * feat: configure kestrel to listen on specified port and update cors policy to use frontend url from configuration
    
    * feat: consolidate ci workflows by adding heroku deployment and removing separate test workflow
    
    * refactor: create app options for port and frontend url configuration

---
+++ backend/ChatAnalyzer.Presentation/Configuration/AppOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatAnalyzer.Presentation.Configuration;
+
+public class AppOptions
+{
+    public string FrontendUrl { get; set; } = string.Empty;
+    public int Port { get; set; } = 8080;
+}
